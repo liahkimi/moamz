a0ac57cdd904bf1c3a952e42a1cf40e86ff50873
taemin1997 <taemin9709@gmail.com>
2024-11-07 21:41:24 +0900

    20241107 고태민 pull 받기전

--- src/main/java/com/example/moamz/mapper/community/receipe/ReceipeMapper.java
+++ src/main/java/com/example/moamz/mapper/community/receipe/ReceipeMapper.java
@@ -1,7 +1,16 @@
 package com.example.moamz.mapper.community.receipe;
 
+import com.example.moamz.domain.dto.community.receipe.ReceipeDetailDTO;
+import com.example.moamz.domain.dto.community.receipe.ReceipeListDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ReceipeMapper {
+//    레시피 디테일
+    ReceipeDetailDTO selectRecipePost(Long fgPostId);
+
+//    레시피 리스트
+    ReceipeListDTO selectRecipePostList();
+
+//    fg_post에 insert
 }
