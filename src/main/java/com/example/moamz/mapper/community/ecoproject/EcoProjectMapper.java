c7ebfe1284cdea9eef7aae82ad227b200fe6cf0d
taemin1997 <taemin9709@gmail.com>
2024-11-08 18:47:41 +0900

    20241108 고태민 pr 받기전

--- src/main/java/com/example/moamz/mapper/community/ecoproject/EcoProjectMapper.java
+++ src/main/java/com/example/moamz/mapper/community/ecoproject/EcoProjectMapper.java
@@ -1,8 +1,32 @@
 package com.example.moamz.mapper.community.ecoproject;
 
+import com.example.moamz.domain.dto.community.PostDTO;
+import com.example.moamz.domain.dto.community.ecoproject.EcoCertDetailDTO;
+import com.example.moamz.domain.dto.community.ecoproject.EcoCertListDTO;
+import com.example.moamz.domain.dto.community.ecoproject.EcoCertWriteDTO;
+import com.example.moamz.domain.dto.community.ecoproject.EcoProjectListDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface EcoProjectMapper {
-    String testNick();
+    
+    // 프로젝트 리스트
+    List<EcoProjectListDTO> ecoProjectList();
+
+    // 인증글 디테일
+    EcoCertDetailDTO ecoCertDetail(Long fgPostId);
+
+    // 인증글 리스트
+     List<EcoCertListDTO> ecoCertList(Long fgProjectId);
+
+     // 인증글에 insert
+    void ecoCertInsert(EcoCertWriteDTO ecoCertWriteDTO);
+
+    // post에 insert(eco 인증)
+    void ecoCertPostInsert(PostDTO postDTO);
+
+    // 인증글 삭제
+    void ecoCertDelete(Long fgPostId);
 }
--- src/main/java/com/example/moamz/mapper/community/receipe/ReceipeMapper.java
+++ src/main/java/com/example/moamz/mapper/community/receipe/ReceipeMapper.java
@@ -1,8 +1,13 @@
 package com.example.moamz.mapper.community.receipe;
 
+import com.example.moamz.domain.dto.community.PostDTO;
 import com.example.moamz.domain.dto.community.receipe.ReceipeDetailDTO;
 import com.example.moamz.domain.dto.community.receipe.ReceipeListDTO;
+import com.example.moamz.domain.dto.community.receipe.ReceipeModifyDTO;
+import com.example.moamz.domain.dto.community.receipe.ReceipeWriteDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ReceipeMapper {
@@ -10,7 +15,16 @@ public interface ReceipeMapper {
     ReceipeDetailDTO selectRecipePost(Long fgPostId);
 
 //    레시피 리스트
-    ReceipeListDTO selectRecipePostList();
+    List<ReceipeListDTO> selectRecipePostList();
 
 //    fg_post에 insert
+    void insertFgPost(PostDTO postDTO);
+
+    // recipe에 insert
+    void insertFgPostRecipe(ReceipeWriteDTO writeDTO);
+
+    // recipe update
+    void updateFgRecipeDetails(ReceipeModifyDTO modifyDTO);
+
+
 }
