0461aff284e6d7cae5cdd1e3835bc20fff46fa36
liahkimi <maruanna1994@gmail.com>
2024-12-04 21:38:34 +0900

    241204 21:38 김가은 대시보드 차트 오류 í수정

--- src/main/resources/static/js/admin/adminDashboard.js
+++ src/main/resources/static/js/admin/adminDashboard.js
@@ -1,28 +1,40 @@
-let myShoppingChart = document.getElementById('shoppingChart');
-let myEcoChart = document.getElementById('ecoCertifyChart');
-
-let myShoppingCht = new Chart(myShoppingChart, {
-  type: 'bar',
-  data: {
-    labels: ['8월', '9월', '10월', '11월'],
-    datasets: [
-      {
-        label: '월별 쇼핑몰 총 구매 건 수 등락 추이',
-        data: [10,20,30,40],
-      }
-    ]
-  },
-});
-
-let myEcoCht = new Chart(myEcoChart, {
-    type: 'bar',
-    data: {
-      labels: ['8월', '9월', '10월', '11월'],
-      datasets: [
-        {
-          label: '월별 에코프로젝트 총 인증글 수 등락 추이',
-          data: [10,20,30,20],
-        }
-      ]
-    },
-  });
+// let myShoppingChart = document.getElementById('shoppingChart');
+// let myEcoChart = document.getElementById('ecoCertifyChart');
+//
+// // 이미 차트 객체가 존재하면 destroy() 호출
+// if (window.myShoppingCht) {
+//   window.myShoppingCht.destroy();
+// }
+//
+// // 쇼핑몰 총 구매 건 수 차트 생성
+// window.myShoppingCht = new Chart(myShoppingChart, {
+//   type: 'bar',
+//   data: {
+//     labels: ['8월', '9월', '10월', '11월'],
+//     datasets: [
+//       {
+//         label: '월별 쇼핑몰 총 구매 건 수 등락 추이',
+//         data: [10, 20, 30, 40],
+//       }
+//     ]
+//   },
+// });
+//
+// // 이미 차트 객체가 존재하면 destroy() 호출
+// if (window.myEcoCht) {
+//   window.myEcoCht.destroy();
+// }
+//
+// // 에코 프로젝트 인증 글 수 차트 생성
+// window.myEcoCht = new Chart(myEcoChart, {
+//   type: 'bar',
+//   data: {
+//     labels: ['8월', '9월', '10월', '11월'],
+//     datasets: [
+//       {
+//         label: '월별 에코프로젝트 총 인증글 수 등락 추이',
+//         data: [10, 20, 30, 20],
+//       }
+//     ]
+//   },
+// });
